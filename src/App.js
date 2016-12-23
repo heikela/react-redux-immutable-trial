9e0d389f967e47e16d35768389309d6c8f64a968
Mikko Heikelä <mikko.heikela@gmail.com>
2016-12-23 00:25:24 +0000

    Clean up create-react-app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Graph } from './Graph';
 import { Provider } from 'react-redux';
 
@@ -10,13 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Provider store={store}>
           <Graph />
         </Provider>
