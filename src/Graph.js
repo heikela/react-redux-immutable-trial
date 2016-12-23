14f2cf89f8e5f8e115e4aded48e6e56aab952b6e
Mikko Heikelä <mikko.heikela@gmail.com>
2016-12-23 00:26:00 +0000

    Tweak the experiment in many ways

--- src/Graph.js
+++ src/Graph.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { List } from '../immutable-js/dist/immutable.js';
+import _ from 'lodash';
 
 class GraphContainer extends Component {
   render() {
-    const { node, level, offset, left, right } = this.props;
+    const { node, offset, walkFunc } = this.props;
     /* eslint-disable no-use-before-define */
-    const elements = graphElementsFromNode(node, offset, level, left, right);
+    const elements = graphElementsFromNode(node, offset, walkFunc);
     /* eslint-enable no-use-before-define */
     return (<g key={offset}>
       {elements}
@@ -20,15 +21,14 @@ class GraphContainer extends Component {
 
 const GraphSegment = (props) => {
   const node = props.node;
-  return (<circle cx={node.x} cy={node.y} r="1" key={props.key}/>);
+  return (<circle cx={node.x} cy={node.y} r="1" fill={node.c} key={props.offset} />);
 };
 
 const graphElementsFromWalk = (walkFunc) => {
   var result = [];
-  walkFunc((item, offset, level, left, right) => {
-  //  console.log('walkFunc called', item, offset, level, left, right);
-    const ElementType = level < 0 ? GraphSegment : GraphContainer;
-    result.push(<ElementType node={item} level={level} offset={offset} left={left} right={right} key={offset} />);
+  walkFunc((item, offset, walkFunc) => {
+    const ElementType = walkFunc ? GraphContainer : GraphSegment;
+    result.push(<ElementType node={item} walkFunc={walkFunc} offset={offset} key={offset}/>);
   });
   return result;
 }
@@ -36,23 +36,44 @@ const graphElementsFromWalk = (walkFunc) => {
 const graphElementsFromList = (list) =>
   graphElementsFromWalk((fn) => list.walkTree(fn));
 
-const walkNode = List.prototype.getWalkNodeFn();
+const graphElementsFromNode = (node, offset, walkFunc) =>
+  graphElementsFromWalk((fn) => walkFunc(fn, node, offset));
 
-const graphElementsFromNode = (node, offset, level, left, right) =>
-  graphElementsFromWalk((fn) => walkNode(fn, node, offset, level, left, right));
+const ticks = (limit) => {
+  const tickSize = [1000000, 200000, 100000, 20000, 10000, 2000, 1000, 200, 100, 10, 1, 0.1, 0.01, 0.001, 0.0001].find(tick => tick < limit);
+  if (!tickSize) return [];
+  return _.range(0, limit, tickSize);
+}
 
 class GraphView extends Component {
   render() {
     const items = this.props.chartItems;
     const lastItem = this.props.prevItem;
     const lastX = lastItem ? lastItem.x : 0;
-    const xLimit = Math.max(100, lastX);
-    const xScale = 95.0/xLimit;
-    const scale = "scale(" + xScale + " 1)";
-//    console.log(items);
+    const xLimit = Math.max(180, lastX);
+    const xScale = 180.0/xLimit;
+//    const transform = "scale(" + xScale + " 1)";
+    const xTranslation = -Math.max(0, lastX - 180);
+    const transform = "translate(" + xTranslation + ")";
+
+    const axisTicks = ticks(xLimit).map(x => {
+      const scaledX = x * xScale;
+      return (<g key={x}>
+        <line x1={scaledX} y1="0" x2={scaledX} y2="10" />
+        <text x={scaledX} y="20" fontSize="10">
+          {x}
+        </text>
+      </g>
+      );
+    });
+
     return (
-      <svg viewBox="0 0 100 100" onClick={this.props.addThousand}>
-      <g stroke="red" fill="none" transform={scale}>
+      <svg width = "600" height="600" viewBox="0 0 200 200" onClick={this.props.addThousand}>
+      <g stroke="black" fill="black" strokeWidth="0.5">
+        <line x1="0" y1="5" x2="180" y2="5" />
+        {axisTicks}
+      </g>
+      <g stroke="none" fill="red" transform={transform}>
         {graphElementsFromList(items)}
         </g>
       </svg>
@@ -62,10 +83,10 @@ class GraphView extends Component {
 
 export const Graph = connect(
   state => ({
-    chartItems: state.chartItems,
-    prevItem: state.prevItem
+    chartItems: state.chart.chartItems,
+    prevItem: state.chart.prevItem
   }),
   dispatch => ({
-    addThousand: () => dispatch({type: 'addThousand'})
+    addThousand: () => dispatch({type: 'addItems', payload: 1000})
   })
 )(GraphView);
--- src/state.js
+++ src/state.js
@@ -1,17 +1,51 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { List } from '../immutable-js/dist/immutable';
 
 const initialState = {
   chartItems: List(),
   prevItem: {
     x: 0,
-    y: 50
-  }
+    y: 100
+  },
 };
+
+const initialTimingState = {
+  prevTiming: window.performance.now(),
+  count: 0
+};
+
+const logPerformance = (state) => {
+  const seconds = (window.performance.now() - state.prevTiming) / 1000;
+  const operations = state.count;
+  const operationsPerSecond = operations / seconds;
+  console.log(operations + " operations in " + seconds + " s = " + operationsPerSecond + " operations/s");
+  return ({
+    prevTiming: window.performance.now(),
+    count: 0
+  });
+};
+
+const timing = (state = initialTimingState, action) => {
+  switch (action.type) {
+    case 'recolor': // fall through
+    case 'addItems': {
+      const newCount = state.count + 1;
+      const stateWithUpdatedCount = {prevTiming: state.prevTiming, count: newCount};
+      if (newCount % 1000 === 0) {
+        return logPerformance(stateWithUpdatedCount);
+      } else {
+        return stateWithUpdatedCount;
+      }
+    }
+    case 'logPerformance': return logPerformance(state);
+    default: return state;
+  }
+}
 
 const genItem = (prevItem) => ({
   x: prevItem.x + 1,
-  y: prevItem.y + Math.random() - 0.5
+  y: prevItem.y + Math.random() - 0.5,
+  c: 'red'
 });
 
 const addItems = (state, count) => {
@@ -23,24 +57,69 @@ const addItems = (state, count) => {
   };
   return ({
     chartItems: items,
-    prevItem: item
+    prevItem: item,
+    timing: state.timing
   });
 };
 
-const reducer = (state = initialState, action) => {
+const recolor = (state) => {
+  const items = state.chartItems;
+  const length = items.size;
+  if (length < 10) return state;
+  const index = Math.floor(Math.random() * (length - 2)) + 1;
+  const item = items.get(index);
+  const updatedItem = {...item, c: item.c === 'red' ? 'blue' : 'red'};
+  return ({
+    chartItems: items.set(index, updatedItem),
+    prevItem: state.prevItem,
+    timing: state.timing,
+  });
+}
+
+const chart = (state = initialState, action) => {
   switch (action.type) {
-    case 'addItem': return addItems(state, 1);
-    case 'addThousand': return addItems(state, 1000);
+    case 'addItems': return addItems(state, action.payload);
+    case 'recolor': return recolor(state);
     default: return state;
   }
 };
 
+const reducer = combineReducers({
+  timing: timing,
+  chart: chart
+});
+
 export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const addPoint = () => {
+const addPoints = (count) => function dispatchAddPoints() {
   store.dispatch({
-    type: 'addItem'
+    type: 'addItems',
+    payload: count
+  });
+}
+
+const dispatchSmoothen = () => {
+  store.dispatch({
+    type: 'recolor'
   });
 };
 
-window.setInterval(addPoint, 10);
+const addAndSmoothen = (count) => function dispatchAddAndSmoothen() {
+  store.dispatch({
+    type: 'addItems',
+    payload: count
+  });
+  store.dispatch({
+    type: 'recolor'
+  });
+}
+
+const dispatchLogPerformance = () => {
+  store.dispatch({
+    type: 'logPerformance'
+  });
+}
+
+//window.setInterval(addPoints(50), 10);
+window.setInterval(addAndSmoothen(10), 10);
+//window.setInterval(dispatchLogPerformance, 5000);
